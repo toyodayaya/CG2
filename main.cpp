a2b4216f3dca1961fe6a516a4122c2371796b27f
toyodayaya <k024g0031@g.neec.ac.jp>
2026-03-09 16:53:12 +0900

    00_02:警告設定まで

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+#pragma warning(push)
+// C4023の警告を見なかったことにする
+#pragma warning(disable:4023)
 #include <Windows.h>
+#pragma warning(pop)
 
 // Windowsアプリでのエントリーポイント
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
