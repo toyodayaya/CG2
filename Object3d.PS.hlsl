7ab059580275585d1e7c837ee6ece01aab37f271
toyodayaya <k024g0031@g.neec.ac.jp>
2026-03-13 20:40:30 +0900

    05_03:平行光源途中まで

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -3,11 +3,20 @@
 struct Material
 {
     float32_t4 color;
+    int32_t enableLighting;
+};
+
+struct DirectionalLight
+{
+    float32_t4 color;
+    float32_t3 direction;
+    float intensity;
 };
 
 ConstantBuffer<Material> gMaterial : register(b0);
 Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
 
 struct PixelShaderOutput
 {
@@ -19,5 +28,16 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     output.color = gMaterial.color * textureColor;
+    
+    if(gMaterial.enableLighting != 0)
+    {
+        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+    }
+    else
+    {
+        output.color = gMaterial.color * textureColor;
+    }
+    
     return output;
 }
